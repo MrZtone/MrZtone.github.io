6a05ac5c80bb3ea64b0e78fa27d1a3bf9bfe96de
MrZtone <ola_stenis@hotmail.com>
2016-02-19 18:30:23 +0100

    Hey

---
+++ index.php
@@ -0,0 +1,29 @@
+ <!DOCTYPE html>
+<html>
+
+	<head>
+		<link rel="Stylesheet" href="stil.css" type="text/css">
+		<meta charset="UTF-8">
+		<title>Ola Steen</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+		<script src="script.js" type="text/javascript"></script>
+
+	</head>
+	<body>
+		<div id="start">
+			
+			<div id="allbuttons">
+				<div id="ONE" class="butn" onclick='showPage();'><p>ONE</p></div>
+				<div id="TWO" class="butn" onclick='showPage();'><p>TWO</p></div>
+				<div id="THREE" class="butn" onclick='showPage();'><p>THREE</p></div>
+				<div id="FOUR" class="butn" onclick='showPage();'><p>FOUR</p></div>
+			</div>
+
+		</div>
+
+		<div id ="main">
+		</div>
+
+	</body>
+
+</html>
---
+++ script.js
@@ -0,0 +1,138 @@
+var extendend =false;
+
+// ***************BUTTONS******************
+
+$(document).ready(function(){
+    $("#ONE").click(function(){
+        $("#main").load("one.php");
+    });
+});
+
+$(document).ready(function(){
+    $("#TWO").click(function(){
+        $("#main").load("two.php");
+    });
+});
+
+$(document).ready(function(){
+    $("#THREE").click(function(){
+        $("#main").load("three.php");
+    });
+});
+
+$(document).ready(function(){
+    $("#FOUR").click(function(){
+        $("#main").load("four.php");
+    });
+});
+
+//*****************************************
+
+function showPage()
+{
+	if (!extendend)
+	{
+
+		var bckgrnd=document.getElementById("start");
+		var bar;
+		for(var i=0;i<bckgrnd.childNodes.length;++i)
+		{
+			if(bckgrnd.childNodes[i].nodeName=="DIV")
+			{
+				bar =bckgrnd.childNodes[i];
+			}
+		}
+		var i=1;
+		var innerinterval;
+
+		moveBar(bar);
+
+		var interval = setInterval(function() 
+		{
+			var height= 100-i;
+			var heightstring= height+"vh";
+			var margn=93-i;
+			var margnstring=margn+"vh";
+
+			bckgrnd.style.height = heightstring;
+			if(i>50)
+			{
+				bar.style.marginTop =margnstring;
+			}
+
+			if(i==15)
+			{
+				innerinterval = setInterval(function()
+				{
+					height= 100-i;
+					heightstring= height+"vh";
+					margn=93-i;
+					margnstring=margn+"vh";
+
+					bckgrnd.style.height = heightstring;
+					if(i>50)
+					{
+						bar.style.marginTop =margnstring;
+					}
+					if(i==90)
+					{
+						extendend=true;
+						showMain(document.getElementById("main"));
+						
+						clearInterval(interval);
+						clearInterval(innerinterval);
+					}
+
+					i++;
+				} , 10);
+			}
+			
+			if(i==90)
+			{
+				extendend=true;
+				showMain(document.getElementById("main"));
+				
+				clearInterval(innerinterval);
+				clearInterval(interval);
+			}
+
+			i=i+1;
+
+		} ,20);
+	}
+}
+
+	function change(obj,col1, col2) 
+	{
+		obj.style.background=col1;
+		obj.style.color=col2;
+	}
+
+	function moveBar(bar)
+	{
+		var i=0;
+		interval = setInterval(function() 
+		{
+			bar.style.marginLeft= 14+i+"vw";
+			if(i==14)
+			{
+				clearInterval(interval);
+			}
+			++i;
+	} , 20);	
+	}
+
+	function showMain(obj)
+	{
+		var i=1;
+		obj.style.visibility="visible";
+		var interval= setInterval(function()
+		{
+			obj.style.opacity=0.01*i+"";
+			if(i==100)
+			{
+				clearInterval(interval);
+			}
+			i++;
+		}, 10);
+	}
